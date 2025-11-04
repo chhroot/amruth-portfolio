5781cb9d90faabe63f0ba7bb3f36c8e76d9e4abd
Amruth Kiran <amruthkiran47@gmail.com>
2025-11-04 11:56:00 +0530

    chore: simple wordings correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ export default function Page() {
             <section id="skills">
                 <div className="flex min-h-0 flex-col gap-y-3">
                     <BlurFade delay={BLUR_FADE_DELAY * 9}>
-                        <h2 className="text-xl font-bold">Skills</h2>
+                        <h2 className="text-xl font-bold">Tricks of Trade</h2>
                     </BlurFade>
                     <div className="flex flex-wrap gap-1">
                         {DATA.skills.map((skill, id) => (
